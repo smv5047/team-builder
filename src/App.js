59289e709db04c1b121c13e4c373f8d7465fb707
smv5047 <smv5047@gmail.com>
2019-09-09 22:54:13 -0400

    Team Members Rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
 
 function App() {
+const [teamMembers, updateTeamMembers] = useState({
+  firstname: 'Michael',
+  lastname: 'Scott'
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        First Name: {teamMembers.firstname}
+        Last Name: {teamMembers.lastname}
+      </div>
     </div>
   );
 }
